1f6224fb44ae28fd85e64faefe5f24f10ac060e2
Arturo Riquelme <alriquelmep@gmail.com>
2026-05-06 22:35:19 -0400

    erwfewrfwef

--- app.config.js
+++ app.config.js
@@ -30,11 +30,12 @@ export default {
       infoPlist: {
         NSCameraUsageDescription:
           'Hop Business necesita acceder a la cámara para permitirte tomar fotos de tu vehículo',
-        NSMicrophoneUsageDescription:
-          'Hop Business necesita acceder al micrófono para grabar el video de confirmación de identidad con Metamap',
+
         NSLocationWhenInUseUsageDescription:
           'Usamos tu ubicación mientras utilizas la app para mostrar viajes y vehículos cercanos.',
+
         UIBackgroundModes: ['fetch', 'remote-notification'],
+
         expoPushNotifications: true,
       },
       // googleServicesFile: './GoogleService-Info.plist',
@@ -42,37 +43,43 @@ export default {
 
     android: {
       package: 'com.hopmobilityapp.hopbusiness',
-      versionCode: 11,
+      versionCode: 10,
       allowBackup: false,
       googleServicesFile: './google-services.json',
       runtimeVersion: '1.0.0',
       compileSdkVersion: 36,
       targetSdkVersion: 36,
+
       config: {
         googleMaps: {
           apiKey: 'TU_GOOGLE_MAPS_API_KEY',
         },
       },
+
       adaptiveIcon: {
         foregroundImage: './assets/images/icon.png',
         backgroundColor: '#ffffff',
       },
+
       splash: {
         image: './assets/images/logo.png',
         resizeMode: 'contain',
         backgroundColor: '#7a5ce7',
       },
+
       blockedPermissions: [
         'android.permission.READ_MEDIA_IMAGES',
         'android.permission.READ_MEDIA_VIDEO',
         'android.permission.READ_MEDIA_AUDIO',
         'android.permission.READ_EXTERNAL_STORAGE',
         'android.permission.WRITE_EXTERNAL_STORAGE',
-      
+
+        'android.permission.FOREGROUND_SERVICE',
         'android.permission.FOREGROUND_SERVICE_LOCATION',
         'android.permission.FOREGROUND_SERVICE_MEDIA_PROJECTION',
         'android.permission.FOREGROUND_SERVICE_MICROPHONE',
       ],
+
       permissions: [
         'android.permission.ACCESS_NETWORK_STATE',
         'android.permission.CAMERA',
@@ -89,6 +96,15 @@ export default {
       'expo-font',
       'expo-document-picker',
       'sentry-expo',
+
+      [
+        'expo-location',
+        {
+          isAndroidForegroundServiceEnabled: false,
+          isAndroidBackgroundLocationEnabled: false,
+        },
+      ],
+
       [
         'expo-notifications',
         {
@@ -103,15 +119,19 @@ export default {
       router: {
         origin: false,
       },
+
       eas: {
         projectId: '0b6b3950-601a-43aa-a548-3cc298d97ec4',
       },
+
       APP_VARIANT: 'BUSINESS',
-      EXPO_METAMAP_API_KEY: 'TU_METAMAP_API_KEY',
-      EXPO_METAMAP_FLOW_ID: 'TU_METAMAP_FLOW_ID',
+
       EXPO_API_URL: 'https://apihop.hopmobilityapp.com/api',
       EXPO_PUBLIC_API_URL: 'https://apihop.hopmobilityapp.com/api',
-      EXPO_PUBLIC_API_URL_MAP: process.env.EXPO_PUBLIC_API_URL_MAP,
+
+      EXPO_PUBLIC_API_URL_MAP:
+        process.env.EXPO_PUBLIC_API_URL_MAP,
+
       sentryDns: 'TU_SENTRY_DSN',
     },
 
